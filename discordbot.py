2a877ba3760f20845835fb91602ffd0c4357201f
gx1285 <78240988+gx1285@users.noreply.github.com>
2021-10-09 18:05:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,15 +1,18 @@
 from discord.ext import commands
-from os import getenv
+import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='di!', help_command=None)
 
 
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+    channel = bot.get_channel(896321426175442964)
+    await channel.send(error_msg)
+    print(error_msg)
+    await ctx.send(f'エラーが発生しました。\n**エラー内容**\n```{error_msg}```\n(開発者に送信しました)')
 
 
 @bot.command()
@@ -17,5 +20,5 @@ async def ping(ctx):
     await ctx.send('pong')
 
 
-token = getenv('DISCORD_BOT_TOKEN')
+token = os.getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
